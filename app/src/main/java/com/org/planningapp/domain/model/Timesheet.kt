71125b0cb6599822abeeb62937fad41d09a243ec
Macawls <josh@macawls.dev>
2023-09-24 16:52:23 +0200

    add timesheet model

---
+++ app/src/main/java/com/org/planningapp/domain/model/Timesheet.kt
@@ -0,0 +1,14 @@
+package com.org.planningapp.domain.model
+import kotlinx.datetime.LocalDateTime
+
+data class Timesheet(
+    val id: String,
+    val userId: String,
+    val categoryId: String,
+    val entryTime: LocalDateTime,
+    val startTime: LocalDateTime,
+    val endTime: LocalDateTime,
+    val description: String,
+    val photoUrl: String?,
+    val createdAt: LocalDateTime,
+)
