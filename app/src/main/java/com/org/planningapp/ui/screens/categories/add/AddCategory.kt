d9ae420588cc1f05f95e66b1933a1e18f1e7f872
Macawls <josh@macawls.dev>
2023-09-28 18:00:37 +0200

    adjust add category screen

--- app/src/main/java/com/org/planningapp/ui/screens/categories/add/AddCategory.kt
+++ app/src/main/java/com/org/planningapp/ui/screens/categories/add/AddCategory.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,6 +25,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -94,7 +99,16 @@ fun AddCategoryScreen(
             OutlinedTextField(
                 value = title.value,
                 label = { Text("Category Name") },
-                onValueChange = { addCategoryViewModel.onTitleChange(it) }
+                onValueChange = { addCategoryViewModel.onTitleChange(it) },
+                modifier = Modifier.fillMaxWidth(),
+                maxLines = 1,
+                keyboardActions = KeyboardActions(
+                    onDone = { submitAddCategory() }
+                ),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Text
+                )
             )
             Spacer(modifier = Modifier.padding(20.dp))
             LoadingButton(
